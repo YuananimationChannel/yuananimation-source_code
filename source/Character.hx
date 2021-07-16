a1370309cd114a0eac79e0bba164782a99f6b727
YuananimationChannel <85840379+YuananimationChannel@users.noreply.github.com>
2021-07-16 15:38:57 +0700

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -95,8 +95,8 @@ class Character extends FlxSprite
                 animation.addByIndices('idle1', 'idle', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
                 animation.addByIndices('idle2', 'idle', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 
-				addOffset("shoot1", 585, 0);
-                addOffset("shoot2");
+				addOffset("shoot1", 583, -6);
+                addOffset("shoot2", 1, -4);
                 addOffset('idle1');
                 addOffset('idle2');
 
