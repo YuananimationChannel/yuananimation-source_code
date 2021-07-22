72f42f83b5b8744ebca21921d3a7ca566e1ea448
TaeYai <TaeYai123@gmail.com>
2021-07-22 21:17:08 +0700

    music

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -79,6 +79,7 @@ class GameOverSubstate extends MusicBeatSubstate
 
 		if (bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.finished)
 		{
+			FlxG.sound.playMusic(Paths.music('gameOver' + stageSuffix));
 			switch (PlayState.SONG.stage)
 		    {
 			case 'yuanstage':
