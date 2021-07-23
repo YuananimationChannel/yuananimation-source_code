211315e1a9224382f6aa6954e65dda633126d7d2
YuananimationChannel <85840379+YuananimationChannel@users.noreply.github.com>
2021-07-23 23:42:32 +0700

    Update Title

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -168,6 +168,12 @@ class TitleState extends MusicBeatState
 		// bg.updateHitbox();
 		add(bg);
 
+		var bgred:FlxSprite = new FlxSprite(-4.9, -1.7).loadGraphic(Paths.image('Yuan/sky','shared'));
+        bgred.antialiasing = true;
+        bgred.scrollFactor.set(0.9, 0.9);
+        bgred.active = false;
+        add(bgred);
+
 		logoBl = new FlxSprite(-800, -100);
 		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
 		logoBl.antialiasing = true;
@@ -177,7 +183,7 @@ class TitleState extends MusicBeatState
 		// logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
-		gfDance = new FlxSprite(FlxG.width * 0.35, FlxG.height * 1.2);
+		gfDance = new FlxSprite(FlxG.width * 0.45, FlxG.height * 1.2);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
@@ -444,7 +450,7 @@ class TitleState extends MusicBeatState
 			remove(ngSpr);
 
 			FlxG.camera.flash(FlxColor.WHITE, 4);
-			FlxTween.tween(logoBl, {x: -150, y: -100}, {ease: FlxEase.expoInOut, startDelay: 0.2});
+			FlxTween.tween(logoBl, {x: -100, y: -100}, {ease: FlxEase.expoInOut, startDelay: 0.2});
 			FlxTween.tween(gfDance, {y: 50}, {ease: FlxEase.expoInOut, startDelay: 0.8});
 			remove(credGroup);
 			skippedIntro = true;
